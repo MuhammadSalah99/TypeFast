a087d66b3b70dece059d433b6cf8d61718e7ece8
MuhammadSalah99 <mohmmedjunide73@gmail.com>
2024-01-22 13:49:51 +0200

    added tailwind, postcss and autoprefixer

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js, ts,tsx}"],
+  theme: {
+    fontFamily: {
+      tajawal: ['Tajawal', 'sans-serif'],
+    },
+  },
+  plugins: [],
+}
